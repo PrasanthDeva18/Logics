7438ab971367cd5ddc08592450dd8aa65119aa4a
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2026-05-26 14:56:50 +0530

    Add obj-own-prop.js to retrieve object properties

---
+++ obj-own-prop.js
@@ -0,0 +1,15 @@
+let obj1 = {
+    name : "prasanth",
+    rollno : 80,
+    value : 50
+}
+
+let obj2 = {
+    name : "Vennila",
+    rollno : 90
+}
+
+let prop = Object.getOwnPropertyNames(obj2)
+
+console.log(prop)
+
