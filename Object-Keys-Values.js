1c722d1fa116a0e76804a38d3d7838667ab6c563
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2026-05-26 14:35:49 +0530

    Add Object-Keys-Values.js to demonstrate object methods

---
+++ Object-Keys-Values.js
@@ -0,0 +1,15 @@
+let obj1 = {
+    name : "prasanth",
+    rollno : 80,
+    value : 50
+}
+
+let obj2 = {
+    name : "Vennila",
+    rollno : 90
+}
+
+let ObjKeys = Object.keys(obj2);
+let ObjValues = Object.values(obj2)
+
+console.log(ObjKeys, ObjValues)
