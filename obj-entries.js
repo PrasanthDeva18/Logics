9d37067fb3ae226b5829de63c0522cfc2ee6735c
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2026-05-26 14:43:48 +0530

    Add output comment for Object.entries example
    Added output comment for clarity.

---
+++ obj-entries.js
@@ -0,0 +1,17 @@
+let obj1 = {
+    name : "prasanth",
+    rollno : 80,
+    value : 50
+}
+
+let obj2 = {
+    name : "Vennila",
+    rollno : 90
+}
+
+let ObjEnteries = Object.entries(obj1)
+
+console.log(ObjEnteries)
+
+// O/P
+[ [ 'name', 'prasanth' ], [ 'rollno', 80 ], [ 'value', 50 ] ]
