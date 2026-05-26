ead7f0c73cc151ab1916af3a0b313f3fa6b16a9e
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2026-05-26 14:27:13 +0530

    Create object-assign.js

---
+++ object-assign.js
@@ -0,0 +1,20 @@
+let obj1 = {
+    name : "prasanth",
+    rollno : 80,
+    value : 50
+}
+
+let obj2 = {
+    name : "Vennila",
+    rollno : 90
+}
+
+let obj = Object.assign(obj1, obj2)
+
+console.log(obj)
+
+// In Object assign have an one target and multiple source 
+// In Object assign used to clone the object keys to new object or empty
+let obj3 = Object.assign({}, obj2)
+
+console.log(obj3)
