b0036db2cb7d2767d318adca60bec1110ee8319e
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2026-06-04 16:25:28 +0530

    Update date-format-symbols.js

--- date-format-symbols.js
+++ date-format-symbols.js
@@ -1,1 +1,14 @@
-
+| Symbol | Meaning          | Example |
+| ------ | ---------------- | ------- |
+| `dd`   | Day (2 digits)   | 04      |
+| `d`    | Day              | 4       |
+| `MM`   | Month (2 digits) | 06      |
+| `MMM`  | Month short      | Jun     |
+| `MMMM` | Month full       | June    |
+| `yy`   | Year (2 digits)  | 26      |
+| `yyyy` | Year (4 digits)  | 2026    |
+| `HH`   | Hour (24-hour)   | 16      |
+| `hh`   | Hour (12-hour)   | 04      |
+| `mm`   | Minutes          | 30      |
+| `ss`   | Seconds          | 45      |
+| `a`    | AM/PM            | PM      |
