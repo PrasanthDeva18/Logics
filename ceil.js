eb4878b71e5f97d4d01beb7b475d774ed562faa6
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2026-08-11 12:09:43 +0530

    Add examples for Math.ceil function in ceil.js
    Add examples demonstrating the usage of Math.ceil in JavaScript, including positive and negative decimals, strings, and null.

---
+++ ceil.js
@@ -0,0 +1,23 @@
+// In the Js Ceil Math function 
+
+// rounds up the number into next largest numbers
+
+Math.ceil(0.95);    // 1
+Math.ceil(4.00);    // 4 (Integers remain unchanged)
+Math.ceil(7.004);   // 8 
+
+// Postive Decimals
+
+Math.ceil(2.1) // 3
+
+// Negative Decimals
+
+Math.ceil(-2.56) // -2
+
+// Strings
+
+Math.ceil("4.3") // 5
+
+Math.ceil("A") // NaN
+
+Math.ceil(null) || Math.ceil("") // 0
